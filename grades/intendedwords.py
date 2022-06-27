f9ed2caf4ea565ce2ea7958ccc8f210d4a5ea44c
xTheQAZx <63619126+xTheQAZx@users.noreply.github.com>
2022-06-27 01:23:56 -0500

    gitignore v9

--- grades/intendedwords.py
+++ grades/intendedwords.py
@@ -10,5 +10,4 @@ def getIntendedWord(criteria):
     for word in intendedWords:
         if criteria == word:
             return intendedWords[word]
-    print(criteria)
     return criteria.lower()
